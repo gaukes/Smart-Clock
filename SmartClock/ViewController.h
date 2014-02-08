32e2e58b4260ef3fcd4b6a0f8e06e7f8aa460d73
Gautham Kesineni <gautham.kesineni.me@gmail.com>
2014-02-08 01:15:08 -0800

    Added more structural content
    Still needs different menus and more functionality

--- SmartClock/ViewController.h
+++ SmartClock/ViewController.h
@@ -10,7 +10,18 @@
 
 @interface ViewController : UIViewController{
     __weak IBOutlet UIImageView *face;
+    int happiness;
+    __weak IBOutlet UILabel *alarmTime;
+    __weak IBOutlet UILabel *alarmTimeZone;
+    __weak IBOutlet UILabel *dialogue;
+    NSDateComponents *components;
 }
 - (void)makeFaceNeutral;
+- (void)makeFacePositive;
+- (void)makeFaceNegative;
+- (void)changeFace;
+- (void)changeFace: (int)happiness;
+- (void)updateTime;
+- (void)updateDialogue: (NSString *)tempDialogue;
 
 @end
--- SmartClock/ViewController.m
+++ SmartClock/ViewController.m
@@ -8,10 +8,6 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
-@end
-
 @implementation ViewController
 
 - (void)viewDidLoad
@@ -20,12 +16,68 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     self.view.backgroundColor = [UIColor whiteColor];
-    [self makeFaceNeutral];
+    
+    NSDate *date = [NSDate date];
+    NSCalendar *cal = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    components = [cal components:(NSHourCalendarUnit | NSMinuteCalendarUnit) fromDate:date];
+    [self updateTime];
+    
+    [self changeFace];
+}
+
+- (void)updateTime
+{
+    int hour = [components hour];
+    int minute = [components minute];
+    NSString *timeZone = @"AM";
+    
+    if (hour >= 12) {
+        hour = hour - 12;
+        timeZone = @"PM";
+    }
+    if (hour == 0) {
+        hour += 12;
+    }
+    
+    NSString *time = [NSString stringWithFormat:@"%i:%i%i", hour, minute/10, minute%10];
+    alarmTime.text = time;
+    alarmTimeZone.text = timeZone;
+}
+
+- (void)updateDialogue: (NSString *)tempDialogue
+{
+    dialogue.text = tempDialogue;
 }
 
 - (void)makeFaceNeutral
 {
     face.image = [UIImage imageNamed: @"neutral_face.png"];
+}
+
+- (void)makeFacePositive
+{
+    face.image = [UIImage imageNamed: @"positive_face.png"];
+}
+
+- (void)makeFaceNegative
+{
+    face.image = [UIImage imageNamed: @"negative_face.png"];
+}
+
+- (void)changeFace
+{
+    [self changeFace: happiness];
+}
+
+-(void)changeFace: (int)tempHappiness
+{
+    if (tempHappiness > 600) {
+        [self makeFacePositive];
+    } else if (tempHappiness > 300) {
+        [self makeFaceNeutral];
+    } else {
+        [self makeFaceNegative];
+    }
 }
 
 - (void)didReceiveMemoryWarning
